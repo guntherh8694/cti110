37ec4514d17789953abb079c8fc99326966d850a
guntherh8694 <99494826+guntherh8694@users.noreply.github.com>
2022-03-06 17:49:51 -0500

    P2LAB1 - Module 4

---
+++ P2LAB1.py
@@ -0,0 +1,15 @@
+user_int = int(input('Enter integer (32 - 126):\n'))
+
+u_float = float(input('Enter float:\n'))
+
+u_char = input('Enter character:\n')
+
+u_string = input('Enter string:\n')
+
+print(user_int, u_float, u_char, u_string)
+
+print(u_string, u_char, u_float, user_int)
+
+user_int = input('Enter integer (32 - 126):\n')
+
+print(user_int)
